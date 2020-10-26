a1344501e8fcdcf41406458689e9b632423c8742
Martin Cao <martin.g.cao@gmail.com>
2020-10-27 10:29:58 +1100

    Add base URL and partner sequence ID to config

--- inc/config.php
+++ inc/config.php
@@ -2,5 +2,9 @@
 
 namespace MOS_Birdsend;
 
+// Birdsend URL Endpoints
+define( 'BASE_URL_CONTACTS', 'https://api.birdsend.co/v1/contacts' );
+
 // Birdsend sequence IDs
 define( 'SEQUENCE_MOS_MEMBERS', 896 );
+define( 'SEQUENCE_MOS_PARTNERS', 922 );
