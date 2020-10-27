7e9fd49a4c02e1feed5ad1078b1973803ffa8030
Martin Cao <martin.g.cao@gmail.com>
2020-10-27 12:35:52 +1100

    Add function get_contact_id

--- inc/functions.php
+++ inc/functions.php
@@ -22,6 +22,29 @@ function subscribe_to_mos_members( int $user_id ) {
     }
 }
 
+function get_contact_id( $email ) {
+    $client = new Client( [
+        'base_uri' => BASE_URL_CONTACTS,
+        'headers' => [
+            'Authorization' => HEADER_AUTH,
+            'Accept' => HEADER_ACCEPT,
+            'Content-type' => HEADER_CONTENT_TYPE,
+        ],
+        'json' => [
+            'search_by' => 'email',
+            'keyword' => $email,
+            'page' => 1,
+            'per_page' => 1,
+        ]
+    ] );
+    
+    $response = $client->get( BASE_URL_CONTACTS );
+    $body = json_decode( $response->getBody());
+    $contact_id = $body['data'][0]['contact_id'];
+    
+    return $contact_id;
+}
+
 function prepare_payload( int $user_id, int $sequence_id ): array {
     $user = \get_user_by( 'id', $user_id );
 
